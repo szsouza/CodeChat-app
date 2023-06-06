5b9dd5baf3954d2e6f0de6e8290afe79f1fd4ab6
Rodrigo Souza <rodrigosouzaprof.ff@gmail.com>
2023-06-06 12:12:45 -0300

    backend utilizando firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/compat/app";
+import env from "react-dotenv";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const apiKey = import.meta.env.VITE_API_KEY_URL;
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_MESSAGINGSENDERID;
+const appId = import.meta.env.VITE_APP_ID;
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
